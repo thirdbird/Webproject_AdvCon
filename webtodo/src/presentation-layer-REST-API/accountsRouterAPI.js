fbe20744e7fd42272fe6142dd46106eba0fb4dc9
thirdbird <andreas@epaa.nu>
2020-03-08 19:22:22 +0100

    added token creation in sign-in, started verifyToken middleware func

--- webtodo/src/presentation-layer-REST-API/accountsRouterAPI.js
+++ webtodo/src/presentation-layer-REST-API/accountsRouterAPI.js
@@ -1,59 +1,61 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const serverSecret = "sdfkjdslkfjslkfd"
 
 module.exports = function ({ accountManager }) {
 
     const router = express.Router()
 
     router.get("/sign-up", function (request, response) {
-		//response the sign up page
-	})
+        //response the sign up page
+    })
 
-	router.get("/sign-in", function (request, response) {
-		//response the sign in page
-	})
+    router.get("/sign-in", function (request, response) {
+        //response the sign in page
+    })
 
-	router.get("/sign-out", function (request, response) {
-		// response sign out page
-	})
+    router.get("/sign-out", function (request, response) {
+        // response sign out page
+    })
 
-	router.get("/", function (request, response) {
-        accountManager.getAllAccounts(function(errors, accounts){
-            if(0 < errors.length){
+    router.get("/", verifyToken, function (request, response) {
+        accountManager.getAllAccounts(function (errors, accounts) {
+            if (0 < errors.length) {
                 response.status(500).end()
-            }else{
+            } else {
                 response.status(200).json(accounts)
             }
         })
-	})
+    })
 
-	router.get('/:username', function (request, response) {
-		const account = { username: request.params.username }
-        accountManager.getAccountByUsername(account, function(errors, account){
-            if(0 < errors.length){
+    router.get('/:username', function (request, response) {
+        const account = { username: request.params.username }
+        accountManager.getAccountByUsername(account, function (errors, account) {
+            if (0 < errors.length) {
                 response.status(500).end()
-            }else if(!account){
+            } else if (!account) {
                 response.status(404).end()
-            }else{
-                response.status(200).json(account)
+            } else {
+                response.status(200).json(account.username)
             }
         })
     })
 
     //POSTS
-    
+
     router.post('/sign-up', function (request, response) {
-		const account = {
-			username: request.body.username,
+        const account = {
+            username: request.body.username,
             password: request.body.password,
             confirmPassword: request.body.confirmPassword
         }
         try {
             accountManager.createAccount(account, function (errors, account) {
-                if(0 < errors.length){
+                if (0 < errors.length) {
                     response.status(400).json(errors)
                 }
-                else{
-                    response.setHeader("Location", "/accounts/"+account.username)
+                else {
+                    response.setHeader("Location", "/accounts/" + account.id)
                     response.status(201).end()
                 }
             })
@@ -63,53 +65,69 @@ module.exports = function ({ accountManager }) {
     })
 
     router.post("/sign-in", function (request, response) {
-		const account = {
-			username: request.body.username,
-			password: request.body.password
+        const account = {
+            username: request.body.username,
+            password: request.body.password
         }
         try {
             accountManager.getAccount(account, function (errors, account) {
-                if(0 < errors.length){
+                if (0 < errors.length) {
                     response.status(400).json(errors)
-                }
-                else{
-                    response.status(200).json(account)
+                } else {
+                    jwt.sign({ account }, serverSecret, (err, token) => {
+                        if (err) {
+                            response.status(500).json(err)
+                        } else {
+                            response.status(200).json({ token, account })
+                        }
+                    })
                 }
             })
-        } catch (error) {
+        } catch (errors) {
             response.status(500).json.end()
         }
     })
 
-    router.put("/:username", function(request,response){
+    router.put("/:username", function (request, response) {
         const account = {
             username: request.body.username
         }
-        accountManager.updateAccountById(account,function(errors,accountExists){
-            if(errors.includes("databaseError")){
+        accountManager.updateAccountById(account, function (errors, accountExists) {
+            if (errors.includes("databaseError")) {
                 response.status(500).end()
-            }else if(0 < errors.length){
+            } else if (0 < errors.length) {
                 response.status(400).json(errors)
-            }else if(!accountExists){
+            } else if (!accountExists) {
                 response.status(404).end()
-            }else{
+            } else {
                 response.status(204).end()
             }
         })
     })
-    
+
 
     //DELETE
 
     router.delete("/:username", function (request, response) {
-		//delete username 
+        //delete username 
     })
 
     router.put("/:username", function (request, response) {
-		//update username info 
+        //update username info 
     })
 
-    
+
+    function verifyToken(request, response, next) {
+        const authorizationHeader = request.get('authorization')
+        const accessToken = authorizationHeader.substr("Bearer ".length)
+        if(typeof authorizationHeader !== 'undefined'){
+            response.status(200).json(accessToken)
+            next()
+        } else {
+            response.status(403).end()
+        }
+    }
+
     return router
 
 }
--- webtodo/src/presentation-layer-REST-API/app.js
+++ webtodo/src/presentation-layer-REST-API/app.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-//const jwt = require('jsonwebtoken')
-
-//Tokens
-
 const awilix = require('../main')
 const app = express()
 
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
 
 app.use('/todolist', awilix.theTodolistRouterAPI)
 app.use('/accounts', awilix.theAccountRouterAPI)
 
 app.listen(8080, function () {
-	console.log('Web application running on 8080')
+	console.log('Web application API running on 8080')
 })
 
